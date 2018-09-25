45d97e61a1ec2d1bdc1d44a1006479168292be52
zzde <zhangxh1997@gmail.com>
2018-09-25 17:18:10 +0800

    f

--- conf/sh/web.sh
+++ conf/sh/web.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 pwd
 sleep 5
-python3 manage.py makemigrations user disease
-python3 manage.py migrate user
-python3 manage.py migrate disease
-python3 manage.py migrate
-# chown -R 1000 media/
+chown -R 1000 /medicine
+su -m appuser -c "python3 manage.py makemigrations user disease"
+su -m appuser -c "python3 manage.py migrate user"
+su -m appuser -c "python3 manage.py migrate disease"
+su -m appuser -c "python3 manage.py migrate"
+# chown -r 1000 media/
 su -m appuser -c "uwsgi uwsgi.ini"
