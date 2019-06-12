10984bdcd36b83ca5ca6a092cc27cf50f5501598
zzde <zhangxh1997@gmail.com>
2019-06-12 15:28:10 +0800

    fix

--- medicine/settings.py
+++ medicine/settings.py
@@ -106,13 +106,13 @@ if os.getenv('USE_MYSQL') == 'true':
             'PORT': os.getenv('MYSQL_PORT'),
         }
     }
-# else:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#         }
-#     }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
